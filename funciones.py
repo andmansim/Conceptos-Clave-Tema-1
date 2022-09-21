004ec37f74b1055936867ac601ab72269e2f624e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-21 16:45:25 +0200

    inicio

--- funciones.py
+++ funciones.py
@@ -2,3 +2,12 @@ class Punto:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
+    def cuadrante(self):
+        pass
+    
+    def vector(self):
+        pass
+    
+    def distancia(self):
+        pass
