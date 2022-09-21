8392cb1b6b86271752bf711a7c315f23cc0ce4c0
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-21 16:52:16 +0200

    definimos los cuadrantes

--- funciones.py
+++ funciones.py
@@ -3,8 +3,27 @@ class Punto:
         self.x = x
         self.y = y
         
+        
     def cuadrante(self):
-        pass
+        if self.x and self.y == 0:
+            print('El punto está en el origen de coordenadas')
+        elif self.x > 0:
+            if self.y > 0:
+                print('El punto está en el primer cuadrante')
+            elif self.y < 0:
+                print('El punto está en el cuarto cuadrante')
+            else:
+                print('El punto está sobre el eje X')
+        elif self.x < 0:
+            if self.y > 0:
+                print('El punto está en el segundo cuadrante')
+            elif self.y < 0:
+                print('El punto está en el tercer cuadrante')
+            else:
+                print('El punto está sobre el eje X')
+        elif self.y == 0:
+            print('Estamos sobre el eje y')
+        
     
     def vector(self):
         pass
