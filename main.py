7c6a753faafc005dfb89d098a3e98f32c39ac403
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-21 17:19:41 +0200

    escribimos los puntos

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from funciones import Punto
 
 if __name__=='__main__':
     punto = Punto()
-    punto.setter(1, 2)
-    c = punto.get()
+    punto.setter(2, 3)
+    A = punto.get()
+    punto.setter(5, 5)
+    B = punto.get()
+    punto.setter(-3, -1)
+    C = punto.get()
+    punto.setter(0,0)
+    D = punto.get()
+    print('Los puntos son: ' + str(A) + ', ' + str(B) + ', ' + str(C) + ' y ' + str(D) )
     #punto.cuadrante()
