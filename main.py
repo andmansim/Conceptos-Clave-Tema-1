0e691de6989dedae9895fe2a414b037f3938e1ca
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-21 17:13:59 +0200

    intento 1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from funciones import Punto
 
 if __name__=='__main__':
-    punto = Punto(1)
-    punto.cuadrante()
+    punto = Punto()
+    punto.setter(1, 2)
+    c = punto.get()
+    #punto.cuadrante()
